caa27f8eecc2ec2f4b127ab6df14fec59847dcde
Giovani Fernandes <giovani_oli@hotmail.com>
2021-06-12 16:46:53 -0400

    feat: adicionado active storage icon payment_method

--- app/models/payment_method.rb
+++ app/models/payment_method.rb
@@ -1,5 +1,6 @@
 class PaymentMethod < ApplicationRecord
   enum kind: {boleto: 1, credit_card: 2, pix: 3}  
+  has_one_attached :icon, dependent: :destroy  
 
   validates :fee, :name, :max_fee, :kind,  presence: true
 end
